6bb4db31913fab2654b1d478572b7f2020b2235a
Phillip Gibson <gibson.phillip.j@gmail.com>
2016-09-30 09:16:45 -0400

    Update link to style.css

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function formatPage (data) {
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="/style.css">
+        <link rel="stylesheet" href="./style.css">
         <title>Title - ${data.date}</title>
       </head>
       <body>
