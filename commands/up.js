b90ee11f2f0d9f6e6e5f9f6fd1b3fe813f432340
Phillip Gibson <pgibson@carekinesis.com>
2017-03-03 22:16:01 -0500

    chalk gen before updating blog. Print push output

--- commands/up.js
+++ commands/up.js
@@ -1,11 +1,13 @@
-const { git } = require('cmd-executor')
+const { chalk, git } = require('cmd-executor')
+const log = console.log
 
 module.exports = async () => {
   try {
+    await chalk.gen()
     await git.add('.')
     await git.commit('-m "Updating blog"')
-    await git.push('origin', 'master')
+    log(await git.push('origin', 'master'))
   } catch (e) {
-    console.log(e)
+    log(e.toString())
   }
 }
